a668822580880710ae31ebe1434505269f16fdaf
Alejandro <alejoprivas@gmail.com>
2019-11-16 18:02:29 -0300

    adding new components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewslistComponent } from './core/components/newslist/newslist.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+
+import {MaterialModule} from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HeaderComponent,
+    NewslistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ client/src/app/core/components/newslist/newslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newslist',
+  templateUrl: './newslist.component.html',
+  styleUrls: ['./newslist.component.sass']
+})
+export class NewslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
