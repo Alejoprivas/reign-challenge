617a0967853911338228f5b84df622e52c1d5d9f
Alejandro <alejoprivas@gmail.com>
2019-11-17 05:23:48 -0300

    fixed containers

--- server/properties.js
+++ server/properties.js
@@ -4,5 +4,5 @@ module.exports = {
     port: process.env.NODE_PORT || 3000,
     scrapUrl:  "https://hn.algolia.com/api/v1/search_by_date?query=nodejs",
     api: process.env.NODE_API != null ? process.env.NODE_API : '/api',
-    seedDB: process.env.seedDB != false ? process.env.seedDB : false
+    seedDB: process.env.seedDB != null ? process.env.seedDB : true
 }
